381528cd0a737164b727d3b50450318cb024f112
lam <491165524@qq.com>
2024-06-16 11:25:13 +0800

    add minor-icon

---
+++ miniprogram/components/minor-item/minor-item.ts
@@ -0,0 +1,31 @@
+// components/minor-item/minor-item.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    src :{
+      type:String,
+      value:""
+    },
+    describe:{
+      type:String,
+      value:""
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
